82d95beedd5eb67ed2e20ea4ab3c1914bb0bd930
Antonio Anon Brasolin <tonhoanhon@gmail.com>
2014-05-24 22:03:42 -0400

    Fix typos resulted from wrong file added

--- mech_datacom.py
+++ mech_datacom.py
@@ -1,94 +1,68 @@
 from neutron.plugins.ml2 import driver_api as api
 
 
-class DatacomMechanismDriver(driver_api.MechanismDriver):
+class DatacomMechanismDriver(api.MechanismDriver):
     """    """
     def __init__(self, rpc=None):
         pass
- 
+
     def initialize(self):
         pass
 
     def create_network_precommit(self, context):
-        """Remember the tenant, and network information."""
+        """Within transaction."""
 
         pass
 
     def create_network_postcommit(self, context):
-        """Provision the network on the Arista Hardware."""
+        """After transaction is done."""
 
-        pass 
+        pass
 
     def update_network_precommit(self, context):
-        """At the moment we only support network name change
+        """Within transaction."""
 
-        Any other change in network is not supported at this time.
-        We do not store the network names, therefore, no DB store
-        action is performed here.
-        """
-        pass 
+        pass
 
     def update_network_postcommit(self, context):
-        """At the moment we only support network name change
+        """After transaction is done."""
 
-        If network name is changed, a new network create request is
-        sent to the Arista Hardware.
-        """
-        pass 
+        pass
 
     def delete_network_precommit(self, context):
-        """Delete the network infromation from the DB."""
-        pass 
+        """Within transaction."""
+
+        pass
 
     def delete_network_postcommit(self, context):
-        """Send network delete request to Arista HW."""
-        pass 
+        """After transaction is done."""
+
+        pass
 
     def create_port_precommit(self, context):
-        """Remember the infromation about a VM and its ports
+        """Within transaction."""
 
-        A VM information, along with the physical host information
-        is saved.
-        """
-        pass 
+        pass
 
     def create_port_postcommit(self, context):
-        """Plug a physical host into a network.
+        """After transaction is done."""
 
-        Send provisioning request to Arista Hardware to plug a host
-        into appropriate network.
-        """
-        pass 
+        pass
 
     def update_port_precommit(self, context):
-        """Update the name of a given port.
+        """Within transaction."""
 
-        At the moment we only support port name change.
-        Any other change to port is not supported at this time.
-        We do not store the port names, therefore, no DB store
-        action is performed here.
-        """
-        pass 
+        pass
 
     def update_port_postcommit(self, context):
-        """Update the name of a given port in EOS.
+        """After transaction is done."""
 
-        At the moment we only support port name change
-        Any other change to port is not supported at this time.
-        """
         pass
- 
+
     def delete_port_precommit(self, context):
-        """Delete information about a VM and host from the DB."""
+        """Within transaction."""
+
         pass
- 
+
     def delete_port_postcommit(self, context):
-        pass 
-
-    def delete_tenant(self, tenant_id):
-        """delete a tenant from DB.
-
-        A tenant is deleted only if there is no network or VM configured
-        configured for this tenant.
-        """
-        pass 
+        pass
