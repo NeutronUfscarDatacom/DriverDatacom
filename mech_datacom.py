fc6287dba2d60ff81990db766c9f39b89b80a769
EttoreZugliani <ettorezugliani@gmail.com>
2014-11-03 15:29:28 -0200

    Remove unintended pdb

--- mech_datacom.py
+++ mech_datacom.py
@@ -26,7 +26,6 @@ config.setup_config()
 class DatacomDriver(api.MechanismDriver):
     """    """
     def __init__(self):
-        import pudb; pudb.set_trace() # BREAKPOINT
         pass
 
     def initialize(self):
